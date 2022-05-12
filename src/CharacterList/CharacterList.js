520c8f55878a18e34bbdcfd2a0076f5d45b1c63e
Khayman King <khaymanak@gmail.com>
2022-05-12 15:24:04 -0700

    Character list mapping is complete

--- src/CharacterList/CharacterList.js
+++ src/CharacterList/CharacterList.js
@@ -4,7 +4,22 @@ import { getCharacters } from '../services/fetch-utils';
 import Character from './Character';
 
 export default function CharacterList() {
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    async function loadCharcters(){
+      const characterData = await getCharacters();
+      setCharacters(characterData);
+    }
+    loadCharcters();
+  }, []);
+
   return (
-    <div>CharacterList</div>
+    <div className='character-list'>
+      {
+        characters.map((character) => 
+          <Character key={character.name} 
+            character={character}/>)
+      }
+    </div>
   );
 }
