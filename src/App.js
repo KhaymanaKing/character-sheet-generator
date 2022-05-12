073615ae06b147f77ee064914a3929223c6ecc0e
Khayman King <khaymanak@gmail.com>
2022-05-12 14:40:58 -0700

    built to auth page now creating auth page

--- src/App.js
+++ src/App.js
@@ -14,12 +14,46 @@ import CreateCharacter from './CreateCharacter/CreateCharacter';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+  const [email, setEmail] = useState('');
+  const [token, setToken] = useState('');
 
-      </header>
-    </div>
+  useEffect(() =>{
+    const user = getUser();
+    if (user) {
+      setToken(user.access_token);
+      setEmail(user.user.email);
+    }
+  }, []);
+
+  async function handleLogout(){
+    await logout();
+    setEmail('');
+    setToken('');
+  }
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1> Currently Logged in as {email}</h1>
+          <nav>
+            <Link to='/'>Characters</Link>
+            <Link to="/create-character">Create A Character</Link>
+          </nav>
+          <button onClick={handleLogout}>Log Out</button>
+        </header>
+        <main>
+          <Switch>
+            <Route exact path='/'>
+              {
+                token
+                  ? <Redirect to='/characters'/>
+                  : <AuthPage setEmail={setEmail} setToken={setToken}/>
+              }
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
