a7b95726d3a93b91d6252d53786df1dad1f3e81e
Khayman King <khaymanak@gmail.com>
2022-05-12 14:47:28 -0700

    handle functions written for auth page

--- src/AuthPage/AuthPage.js
+++ src/AuthPage/AuthPage.js
@@ -1,6 +1,33 @@
 import React from 'react';
+import { useState } from 'react';
+import { signUp } from '../services/fetch-utils';
+import { getUser, signIn, signOut } from './services/fetch-utils.js';
 
-export default function AuthPage() {
+export default function AuthPage({ setEmail, setToken }) {
+  const [formEmail, setFormEmail] = useState('');
+  const [formPassword, setFormPassword] = useState('');
+
+  async function handleSignUp(e){
+    e.preventDefault();
+    await signUp(formEmail, formPassword);
+    const {
+      access_token,
+      user: { email }
+    } = getUser();
+    setEmail(email);
+    setToken(access_token); 
+  }
+
+  async function handleSignIn(e){
+    e.preventDefault();
+    await signIn(formEmail, formPassword);
+    const {
+      access_token,
+      user: { email }
+    } = getUser();
+    setEmail(email);
+    setToken(access_token); 
+  }
   return (
     <div>AuthPage</div>
   );
