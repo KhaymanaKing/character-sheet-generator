48a4e8f523fd44a50dd0253ee07f2925c2e056de
Khayman King <khaymanak@gmail.com>
2022-05-12 14:27:28 -0700

    fetch utils finished

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -17,11 +17,31 @@ export async function signIn(email, password) {
   if (error) return error;
   return user;
 }
+export async function logout(){
+  await client.auth.signOut();
+  return window.location.href = '../';
+}
 
 export async function createCharacter(character) {
   const { body, error } = await client
     .from ('character_gen')
     .insert (character);
   return error || body;
+}
+
+export async function getCharacters() {
+  const response = await client
+    .from('character_gen')
+    .select();
+
+  return checkError(response);
+}
+
+export async function getCharacterById(id) {
+  const response = await client
+    .from ('character_gen')
+    .match ({ id })
+    .single();
+  return checkError(response);
 }
 
